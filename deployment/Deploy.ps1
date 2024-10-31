fe02445664a5f208cbedd75d130452f0a5f49af0
esans-Wolk <esansberro@wolksoftcr.com>
2024-10-31 14:41:01 -0300

    chore:  reglas firewall sql server ip publica

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -517,7 +517,7 @@ az sql server create --name $SQLServerName --resource-group $ResourceGroupForDep
 Write-host "      ➡️ Set minimalTlsVersion to 1.2"
 az sql server update --name $SQLServerName --resource-group $ResourceGroupForDeployment --set minimalTlsVersion="1.2"
 Write-host "      ➡️ Add SQL Server Firewall rules"
-az sql server firewall-rule create --resource-group $ResourceGroupForDeployment --server $SQLServerName -n AllowAzureIP --start-ip-address "0.0.0.0" --end-ip-address "0.0.0.0" --output $azCliOutput
+az sql server firewall-rule create --resource-group $ResourceGroupForDeployment --server $SQLServerName -n AllowAzureIP --start-ip-address "$publicIp" --end-ip-address "$publicIp" --output $azCliOutput
 if ($env:ACC_CLOUD -eq $null){
     Write-host "      ➡️ Running in local environment - Add current IP to firewall"
 	$publicIp = (Invoke-WebRequest -uri "https://api.ipify.org").Content
